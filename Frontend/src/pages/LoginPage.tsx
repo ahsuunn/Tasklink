e52cd3d0b38ab1f893c26dd9d1f59af7e5f907aa
muhammad aufa <iniabi838@gmail.com>
2024-08-26 21:49:06 +0700

    fix adding logo to Login page

--- Frontend/src/pages/LoginPage.tsx
+++ Frontend/src/pages/LoginPage.tsx
@@ -7,9 +7,10 @@ import { handleFetchError } from "../lib/actions/HandleError";
 import CustomAxios from "../lib/actions/CustomAxios";
 import { IUserForm } from "../lib/types/User";
 import { CurrentUserContext } from "../lib/contexts/CurrentUserContext";
-import Image1 from '/Logindecor1.png'
-import Image2 from '/Logindecor2.png'
-import Image3 from '/Logindecor3.png'
+import Image1 from "/Logindecor1.png";
+import Image2 from "/Logindecor2.png";
+import Image3 from "/Logindecor3.png";
+import logopic from "./images/logo.png";
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -45,13 +46,13 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex flex-col min-h-screen w-screen items-center justify-start px-4 py-12 mt-2 bg-gradient-to-t from-[#7fa1c2] to-[#f0f1f3]">
+    <div className="mt-2 flex min-h-screen w-screen flex-col items-center justify-start bg-gradient-to-t from-[#7fa1c2] to-[#f0f1f3] px-4 py-12">
       <form
         action="post"
         onSubmit={onSubmit}
-        className="bg-[#4679a8] mt-12 mb-4 mx-auto flex h-fit w-full max-w-[500px] flex-col items-center gap-3 rounded-lg py-8 shadow-lg md:px-8"
+        className="mx-auto mb-4 mt-12 flex h-fit w-full max-w-[500px] flex-col items-center gap-3 rounded-lg bg-[#4679a8] py-8 shadow-lg md:px-8"
       >
-        <h1 className="text-white text-center text-3xl mb-2 ">TaskLink</h1>
+        <img src={logopic} alt="Logo" width="50%" />
         <InputText
           value={formData.email as string}
           onChange={onChange}
@@ -65,22 +66,26 @@ const LoginPage = () => {
           placeholder="Password"
           className="py-2"
         />
-        <Button className="bg-white text-black px-6 mt-2 py-1 rounded-xl">Log In</Button>
+        <Button className="mt-2 rounded-xl bg-white px-6 py-1 text-black">
+          Log In
+        </Button>
       </form>
       <div className="flex flex-col items-center">
-        <p className="text-black text-center">Don't have an account yet?</p>
-        <p className="text-black text-center">Make yours here for free.</p>
+        <p className="text-center text-black">Don't have an account yet?</p>
+        <p className="text-center text-black">Make yours here for free.</p>
         <NavLink to="/auth/sign-up">
-        <button className="bg-[#0c173d]  my-2 mx-auto px-6 py-1 rounded-xl text-white">Sign Up</button>
+          <button className="mx-auto my-2 rounded-xl bg-[#0c173d] px-6 py-1 text-white">
+            Sign Up
+          </button>
         </NavLink>
       </div>
       <div>
-        <div className="absolute left-0 bottom-0">
+        <div className="absolute bottom-0 left-0">
           <img src={Image2} alt="Image2"></img>
           <img src={Image1} alt="Image1"></img>
         </div>
-        <div className="absolute right-0 bottom-0">
-        <img src={Image3} alt="Image3"></img>
+        <div className="absolute bottom-0 right-0">
+          <img src={Image3} alt="Image3"></img>
         </div>
       </div>
     </div>
